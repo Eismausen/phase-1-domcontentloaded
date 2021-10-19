e336e47de66e010a450494222c2b7f85689dc12c
David Avery <eismausen@gmail.com>
2021-10-19 10:47:36 -0700

    commit tree 1b77ea61bbdddd354bc32d0088fb2680de040622

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const pTarget = document.getElementById("text");
+document.addEventListener("DOMContentLoaded", textChange);
+
+function textChange() {
+    console.log("The DOM has loaded - initiating text change!");
+    pTarget.textContent = "This is really cool!";
+}
